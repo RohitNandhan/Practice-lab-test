ec754e2d90c33210ea5df1f9f3b2c50770228950
RohitNandhan <98014469+RohitNandhan@users.noreply.github.com>
2022-04-07 22:33:40 +0530

    Add files via upload

---
+++ Pattern/pattern1.java
@@ -0,0 +1,33 @@
+package Practicals;
+
+public class pattern1 {
+	public static void main(String[] args) {
+		int count=4;
+		for( int i=0;i<4;i++) {	
+			for(int j=1;j<=count;j++) {
+				System.out.print(j); }
+			for(int r=0;r<(2*i);r++) {
+				System.out.print("*");
+			}
+				for(int k=count;k>=1;k--) {
+					System.out.print(k);
+				}System.out.println();
+				count--;
+				}
+				} } 
+
+				
+
+					
+				
+	
+			
+		
+			
+				
+			
+		
+		
+	
+
+
---
+++ Pattern/pattern2.java
@@ -0,0 +1,19 @@
+package Practicals;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				int k=1;
+				k=j*i;
+				System.out.print(k);
+			}System.out.println();
+		}	
+	
+	}
+
+}
